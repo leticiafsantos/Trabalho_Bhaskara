b00bb110e82a27bb4c16da394e366ebf7bdd0ca1
Sérgio Lacerda <sergio.lacerda@sp.senai.br>
2024-08-23 10:49:30 -0300

    Adicionando Arquivos do Projeto

---
+++ BhaskaraApp/Bhaskara.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BhaskaraApp
+{
+    public class Bhaskara
+    {
+        // Coeficientes da equação de segundo grau
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        // Construtor com inicialização dos coeficientes
+        public Bhaskara(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        // Calculando o valor de Delta
+        private double CalcularDelta()
+        {
+            return B * B + 4 * A * C;
+        }
+
+        // Verificando se a equação possui raízes reais
+        public bool TemRaizesReais()
+        {
+            double delta = CalcularDelta();
+            return delta > 0;
+        }
+
+        // Método para calcular as raízes da equação
+        public (double?, double?) CalcularRaizes()
+        {
+            if (!TemRaizesReais())
+            {
+                // Se não tem raízes reais, retorna dois nulos
+                return (null, null);
+            }
+
+            double delta = CalcularDelta();
+            double raizDelta = Math.Sqrt(delta);
+
+            // Calculando as raízes
+            double x1 = (-B + raizDelta) / (2 * C);
+            double x2 = (-B - raizDelta) / (2 * C);
+
+            return (x1, x2);
+        }
+    }
+}
---
+++ BhaskaraApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using BhaskaraApp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Exemplo de uso da classe Bhaskara
+        var bhaskara = new Bhaskara(1, -3, 2);
+
+        if (bhaskara.TemRaizesReais())
+        {
+            var (raiz1, raiz2) = bhaskara.CalcularRaizes();
+            Console.WriteLine($"Raiz 1: {raiz1}");
+            Console.WriteLine($"Raiz 2: {raiz2}");
+        }
+        else
+        {
+            Console.WriteLine("A equação não possui raízes reais.");
+        }
+    }
+}
